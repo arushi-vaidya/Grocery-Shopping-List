b00849efdbc33c992f070e401c3a5c8864ee9650
Arushi Vaidya <153972207+arushi-vaidya@users.noreply.github.com>
2024-02-22 17:49:05 +0530

    Add files via upload

---
+++ Grocery List Management.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+class list{
+	public:
+		int codeno[50];
+		int price[50];
+		char name[30][50];
+		int status[50];
+		int quantity[50];
+		int amount ;
+		int count = 0;
+		void add()
+		{
+			cout << "Enter item code: " << endl;
+			cin >> codeno[count];
+			cout << "Enter item name: " << endl;
+			cin >> name[count];
+			cout << "Enter price: " << endl;
+			cin >> price[count];
+			cout << "Enter quantity: " << endl;
+			cin >> quantity[count];
+			status[count] = 1;
+			count ++;
+			cout <<"\n \n \n" << endl;
+		}
+		void displaytotal()
+		{
+			amount = 0;
+			for (int i=0;i<count;i++)
+			{
+				if (status[i] != 0)
+				{
+					amount = amount + quantity[i]*price[i];
+				}
+			}
+			cout << "Total amount = Rs." << amount << endl;
+			cout <<"\n \n \n" << endl;
+		}
+		void display()
+		{
+			cout <<"CodeNo \t Item Name \t Quantity \t Price Per Unit \t Total Price \t Status" << endl;
+			for (int i=0;i<count;i++)
+			{
+				cout << codeno[i] <<"\t" << name[i] << "\t" << quantity[i] << "\t" << price[i] <<  "\t" << "\t" << price[i]*quantity[i] << status[i] << endl;
+			}
+			cout << "Status = 1 --> Active, Status = 0 --> Inactive" << endl;
+			cout <<"Total items =" << count << endl;
+			displaytotal();
+		}
+		void remove()
+		{
+			int x,i;
+			cout << "Enter code no to delete item " << endl;
+			cin >> x;
+			for (int i=0;i<count;i++)
+			{
+				if (codeno[i] == x)
+				{
+					status[i] = 0;
+				}
+				else cout <<"Item does not exist" << endl;
+			}
+			cout <<"\n \n \n" << endl;
+		}
+		
+};
+
+int main()
+{
+	list ob;
+	int x;
+	do {
+		cout <<"Choose \n 1.Add \n 2.Delete \n 3.Display list \n 4.Show total \n 5 Exit" << endl;
+	cin >> x;
+	switch (x)
+		{
+			case 1:
+				{
+					ob.add();
+					break;
+				}
+			case 2:
+				{
+					ob.remove();
+					break;
+				}
+			case 3:
+				{
+					ob.display();
+					break;
+				}	
+			case 4:
+				{
+					ob.displaytotal();
+					break;
+				}	
+			case 5:
+				{
+					break;
+				}
+			default: cout <<"Wrong value" << endl;
+		}
+	} while (x!=5);
+}
